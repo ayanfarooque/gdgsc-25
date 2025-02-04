a7c8b0f0d06be549a50d9f5ae9ad8d971c8252bb
venom_1404 <ronitdhase@gmail.com>
2025-02-05 00:04:06 +0530

    newspage updated

--- client/src/components/NewsResourcesComponent/Card.jsx
+++ client/src/components/NewsResourcesComponent/Card.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 const Card = ({ newsId, newsHeading, newsContent, newsImage }) => {
   const navigate = useNavigate();
 
+
   return (
     <div 
       className="p-4 transition-transform transform hover:scale-105 border rounded-2xl border-black bg-gray-50 shadow-md h-20 w-full cursor-pointer"
@@ -15,4 +16,5 @@ const Card = ({ newsId, newsHeading, newsContent, newsImage }) => {
 };
 
 export default Card;
+
 
