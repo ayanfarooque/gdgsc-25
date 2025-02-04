55e7763a3f0e9d39badd3ed661519890aaa53a51
venom_1404 <ronitdhase@gmail.com>
2025-02-04 18:34:59 +0530

    assignment page added

---
+++ client/src/components/AssignmentComponent/SubmittedAssignments.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SubmittedAssignments = () => {
+    return (
+        <div className="bg-[#29b6c6] p-6 rounded-lg text-white w-1/2">
+        <h2 className="text-lg font-bold mb-4">SUBMITTED ASSIGNMENTS</h2>
+        <p>17 Feb - SST - AS1002</p>
+        <p>19 Feb - SCI - AS1003</p>
+        <p>21 Feb - MATH - AS1006</p>
+      </div> 
+    )
+}
+
+export default SubmittedAssignments
