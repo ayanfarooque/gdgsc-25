07329f606dd9ae37e0aa9e602d4d5fd57b8d7626
ronit-1404 <88834510+ronit-1404@users.noreply.github.com>
2025-03-05 12:30:13 +0530

    idfixes

--- server/controllers/testResultController.js
+++ server/controllers/testResultController.js
@@ -1,27 +1,20 @@
 const TestResult = require('../models/TestResult.js')
-const Student = require('../models/Student.js')
-const Teacher = require('../models/Teacher.js')
 
-exports.getTestScore = async (req,res) => {
+exports.getAllTestScores = async (req, res) => {
     try {
-        const { studentId } = req.params; // Ensure studentId is correctly extracted
+        const testScores = await TestResult.find()
+            .populate("studentId", "name studentId class")
+            .populate("subjectId", "subjectName")
+            .populate("testId", "testName")
+            .sort({ CreatedAt: -1 });
 
-        if (!studentId) {
-            return res.status(400).json({ message: "Student ID is required" });
+        if (!testScores || testScores.length === 0) {
+            return res.status(404).json({ message: "No test scores found" });
         }
-        const testScores =  await TestResult.find({studentId})
-            .populate("studentId", "name studentId class")  
-            .populate("subjectId", "subjectName")           
-            .populate("testId", "testName")                 
-            .sort({ CreatedAt: -1 });                       
 
-            if(!testScores || testScores.length === 0){
-                return res.status(404).json({message: "No test scores found"})
-            }
-
-            res.status(200).json(testScores);
+        res.status(200).json(testScores);
     } catch (error) {
         console.error("Error fetching test scores:", error);
         res.status(500).json({ message: "Internal Server Error" });
     }
-}
+};
