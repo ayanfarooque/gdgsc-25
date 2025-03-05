f2df5c74e857350cf864257f74684bd08b5d8f26
ronit-1404 <88834510+ronit-1404@users.noreply.github.com>
2025-03-05 15:38:14 +0530

    notification route fixes

--- server/routes/notificationsRoutes.js
+++ server/routes/notificationsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const notificationController = require('../controllers/notificationController.js')
 
 router.post('/create-notification',notificationController.createNotification)
-router.post('/all-notifications',notificationController.getAllNotifications)
+router.get('/all-notifications',notificationController.getAllNotifications)
 router.get('/notification/student/:studentId',notificationController.getNotificationsByStudent)
 router.get('/notification/teacher/:teacherId',notificationController.getNotificationsByTeacher)
 router.delete('/notificaion/delete/:notificationId',notificationController.deleteNotification)
