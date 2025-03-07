876014c09d7b1a7b84b1036eaa83fe01efc300e4
ronit-1404 <88834510+ronit-1404@users.noreply.github.com>
2025-03-07 22:05:03 +0530

    student model update

--- server/models/Student.js
+++ server/models/Student.js
@@ -1,46 +1,56 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const studentSchema = new mongoose.Schema({
+const studentSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true },
-    StudentId: {type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    address: [{
-        line1: {type: String, required: true},
-        line2: {type: String},
-        city:  {type: String, required: true},
-        state: {type: String, required: true},
-        postalcode: {type: String, required: true}
-    }],
+    address: {
+      line1: { type: String, required: true },
+      line2: { type: String },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      postalCode: { type: String, required: true }
+    },
     dob: { type: Date, required: true },
-    grade: { type: String },
+    grade: { type: String, required: true },
     growthPoints: { type: Number, default: 0 },
-    guardianName: { type: String }, 
+    guardianName: { type: String },
 
-    subjects: [{
+    subjects: [
+      {
         subjectId: { type: mongoose.Schema.Types.ObjectId, ref: "Subject" },
         subjectName: { type: String }
-    }],
+      }
+    ],
 
-    isWeakStudent: { type: Boolean, default: false }, 
-    istopStudent: { type: Boolean, default: false }, 
+    isWeakStudent: { type: Boolean, default: false },
+    isTopStudent: { type: Boolean, default: false },
 
-    teachers: [{
-        teacherId: { type: mongoose.Schema.Types.ObjectId, ref: "Teacher"},
+    teachers: [
+      {
+        teacherId: { type: mongoose.Schema.Types.ObjectId, ref: "Teacher" },
         teacherName: { type: String },
-        subjectId: { type: mongoose.Schema.Types.ObjectId, ref: "Subject"}
-    }],
+        subjectId: { type: mongoose.Schema.Types.ObjectId, ref: "Subject" }
+      }
+    ],
 
-    testScores: [{
+    testScores: [
+      {
         subjectId: { type: mongoose.Schema.Types.ObjectId, ref: "Subject" },
         subjectName: { type: String },
-        date: { type: Date, default: () => Date.now() }, 
+        date: { type: Date, default: () => Date.now() },
         score: { type: Number },
         total: { type: Number }
-    }],
+      }
+    ],
 
     badges: [{ type: String }]
-}, { timestamps: true });
+  },
+  { timestamps: true }
+);
+
+
 
 module.exports = mongoose.model("Student", studentSchema);
